ba27bbd3e3bea09bfa441b7d558ae00e7492d085
Sylvain Viart <sviart@cloudiway.com>
2021-04-20 10:18:09 +0200

    remove 404 test from werkzeug.exceptions
    some working 404 custom handling

--- examples/error_handling/custom_error_handler.py
+++ examples/error_handling/custom_error_handler.py
@@ -1,38 +1,26 @@
 from flask import Flask
 from flask_restx import Resource, Api
-from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 api = Api(app)
 
+# this will disable flask_restx default error message
+# See: https://flask-restx.readthedocs.io/en/latest/errors.html?highlight=ERROR_INCLUDE_MESSAGE#the-api-errorhandler-decorator
 app.config['ERROR_INCLUDE_MESSAGE'] = False
+
+# could also be useful
 #app.config['ERROR_404_HELP'] = False
 #app.config['ERROR_INCLUDE_MESSAGE'] = False
 #app.config['RESTX_ERROR_404_HELP'] = False
 
 
 # you have to define your own Exception as flask_restx doesn't provide them.
-class NoResultFound(Exception):
-    pass
-
 class RootException(Exception):
-    pass
-
-class CustomException(Exception):
-    pass
-
-class AnotherException(Exception):
-    pass
-
-class FakeException(Exception):
     pass
 
 class NoResultFound(Exception):
     def __init__(self, name_item_not_found):
         self.item_not_found = name_item_not_found
-
-class URLNotFound(NotFound):
-    pass
 
 # now define your own errorhandler function with custom handling
 @api.errorhandler(RootException)
@@ -40,30 +28,15 @@ def handle_root_exception(error):
     '''Return a custom message and 400 status code'''
     return {'message': 'What you want'}, 400
 
-@api.errorhandler(CustomException)
-def handle_custom_exception(error):
-    '''Return a custom message and 400 status code'''
-    return {'message': 'What you want'}, 400
-
-@api.errorhandler(AnotherException)
-def handle_another_exception(error):
-    '''Return a custom message and 500 status code'''
-    return {'message': error.specific}
-
-@api.errorhandler(FakeException)
-def handle_fake_exception_with_header(error):
-    '''Return a custom message and 400 status code'''
-    return {'message': error.message}, 400, {'My-Header': 'Value'}
-
 @api.errorhandler(NoResultFound)
 def handle_no_result_exception(error):
     '''Return a custom not found error message and 404 status code'''
     return {'message': "your request was not found: '%s'" % error.item_not_found }, 404
 
-
-@api.errorhandler(NotFound)
-def handle_wrong_api_URL(error):
-    return {'message': "your request was not found: '%s'" % error }, 404
+# for global 404 error handling, use app decorator:
+@app.errorhandler(404)
+def handle_wrong_api_URL(e):
+    return {'message': "your request was not found: '%s'" % e }, 404
 
 # now your routes
 @api.route('/hello')
